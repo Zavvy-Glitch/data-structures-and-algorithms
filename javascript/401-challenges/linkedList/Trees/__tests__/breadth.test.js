39b993e898e9f0cc91d487b18133bce030823652
chattray <chea0073@gmail.com>
2021-11-10 14:34:49 -0800

    testing

--- javascript/401-challenges/linkedList/Trees/__tests__/breadth.test.js
+++ javascript/401-challenges/linkedList/Trees/__tests__/breadth.test.js
@@ -1,4 +1,4 @@
-const { Node, Tree } = require('../trees.js');
+const { Node, Tree } = require('../breadthTrees.js');
 
 test('Node is a class', () => {
   expect(typeof Node.prototype.constructor).toEqual('function');
@@ -7,32 +7,26 @@ test('Tree is a class', () => {
   expect(typeof Tree.prototype.constructor).toEqual('function');
 });
 test('Can successfully instantiate an empty tree', () => {
-  const n = new Node();
   expect(() => {
   }).not.toThrow();
 });
 test('Can return an array of nodes ensuring Breadth First construction', () => {
-  const n = new Node();
   expect(() => {
   }).not.toThrow();
 });
 test('Can replace a value with `Fizz` if value is divisible by 3', () => {
-  const n = new Node();
   expect(() => {
   }).not.toThrow();
 });
 test('Can replace a value with `Buzz` if value is divisible by 5', () => {
-  const n = new Node();
   expect(() => {
   }).not.toThrow();
 });
 test('Can replace a value with `FizzBuzz` if value is divisible by 3 & 5', () => {
-  const n = new Node();
   expect(() => {
   }).not.toThrow();
 });
 test('If the value is not divisible by 3 nor 5, return the value as a string', () => {
-  const n = new Node();
   expect(() => {
   }).not.toThrow();
 });
