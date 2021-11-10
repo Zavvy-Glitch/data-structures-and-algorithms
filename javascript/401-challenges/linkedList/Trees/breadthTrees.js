37ecf04afd1da78a2a040bd8974c819fb16cf9eb
chattray <chea0073@gmail.com>
2021-11-10 14:25:50 -0800

    fizzBuzz tree construction added

--- javascript/401-challenges/linkedList/Trees/breadthTrees.js
+++ javascript/401-challenges/linkedList/Trees/breadthTrees.js
@@ -16,18 +16,34 @@ class Tree {
 
 function breadthFirst(root) {
   const array = [];
-
+  let result = [];
   let current = root;
   array.unshift(root);
   while(array.length){
     current = array.pop();
-
-    console.log(current.value);
-
+    result.push(current.value);
     for(let node of current.children) {
       array.unshift(node);
     }
   }
+  return result;
+}
+
+function fizzBuzzTree(node) {
+  let result = [];
+  let i;
+
+  for (i = 0; i < node.length; i++) {
+    if((node[i]%15) === 0)
+      result.push('FizzBuzz');
+    else if ((node[i]%3) === 0)
+      result.push('Fizz');
+    else if ((node[i]%5) === 0)
+      result.push(' Buzz');
+    else
+      result.push(node[i].toString());
+  }
+  return result;
 }
 
 let node = new Tree(2);
@@ -37,11 +53,11 @@ node.root.children.push(new Node(8), new Node(12));
 node.root.children[0].children.push(new Node(16));
 node.root.children[0].children[0].children.push(new Node(64), new Node(50));
 node.root.children[1].children.push(new Node(35));
-node.root.children[1].children[0].children.push(new Node(83));
+node.root.children[1].children[0].children.push(new Node(83), new Node(135));
 node.root.children[1].children[0].children[0].children.push(new Node(6), new Node(4));
 
-breadthFirst(node.root);
+
+console.log(breadthFirst(node.root));
+console.log(fizzBuzzTree(breadthFirst(node.root)));
 
 module.exports = { Node, Tree };
-
-
