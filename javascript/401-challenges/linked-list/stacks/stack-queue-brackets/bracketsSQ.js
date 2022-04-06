9fccff06ab3db348e71ef11c8fd0746892b93a72
Chattray <chattray.chea@gmail.com>
2022-04-06 15:40:05 -0700

    stacks and queue brackets implementation w/ tests

--- javascript/401-challenges/linked-list/stacks/stack-queue-brackets/bracketsSQ.js
+++ javascript/401-challenges/linked-list/stacks/stack-queue-brackets/bracketsSQ.js
@@ -1,12 +1,39 @@
-const { Queue } = require('../QueueStacks/stacksAndQueues.js');
+const { Stack } = require('../QueueStacks/stacksAndQueues.js');
+
+//note: Solution with help from LeetCode
 
 class Brackets {
   constructor() {
-    this.round = new Queue();
-    this.square = new Queue();
-    this.curly = new Queue();
+    this.stack = new Stack
   }
 
-}
+  validateBrackets = (brackets) => {
+   if(brackets.length % 2 !== 0) return false;
+   const map = {
+     '(' : ')',
+     '[' : ']',
+     '{' : '}',
+   }
+    
+    for (const bracket of brackets) {
+
+      if(map.hasOwnProperty(bracket)) {
+        this.stack.push(bracket)
+      } else {
+        const closeBracket = this.stack.pop();
+        if(bracket !== map[closeBracket]){
+          return false
+        }
+      }
+    }
+   return this.stack.isEmpty();
+  };
+};
+
+let vb = new Brackets();
+
+console.log(vb.validateBrackets('([apple])'))
+
+
 
 module.exports = Brackets;
