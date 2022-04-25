94a2ff8bc71999cf3ba93fa88230e6ce36965355
Chattray <chattray.chea@gmail.com>
2022-04-25 16:25:41 -0700

    selection sort added

--- javascript/401-challenges/insertSort/insertSort.js
+++ javascript/401-challenges/insertSort/insertSort.js
@@ -13,8 +13,27 @@ function insertSort(arr) {
   }
 }
 
+function selectionSort(arr) {
+  let n = arr.length;
+  for(let i = 0; i < n; i++) {
+    let min = i;
+    for(let j = i + 1; j < n; j++){
+      if(arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
+
 let arr = [8,4,23,42,16,15];
 insertSort(arr);
-// console.log(arr);
+selectionSort(arr);
+console.log(arr);
 
 module.exports = { insertSort };
